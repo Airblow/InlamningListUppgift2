830b6034192f8113bb07d568bb983200bc0503f5
Airblow1 <143172177+Airblow@users.noreply.github.com>
2024-02-03 17:09:01 +0100

    Add project files.

---
+++ InlamningListUppgift2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InlamningListUppift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> {3, 15, 6, 11, 54, 19, 22, 57};
+            Console.WriteLine(Max(numbers));
+
+            Console.ReadKey();
+
+        }
+
+        static int Max(List<int> numbers)
+        {
+            int largestNumber = 0;
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > largestNumber)
+                {
+                    largestNumber = numbers[i];
+                }
+
+            }
+
+            return largestNumber;
+
+        }
+
+    }
+
+}
